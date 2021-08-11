c7f3a6d6ec0fa24d3f30dd0237739b89be823554
Ruslan Khasbulatov <ruslan.khasbulatov@redhat.com>
2021-08-11 12:39:59 -0400

    checkstyle fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,31 +1,33 @@
 package com.example;
 
 /**
- * Graceful termination demo
+ * Graceful termination demo.
  */
 public final class App {
 
     private static Thread main;
 
+    private static final long THOUSAND = 1000;
+
+    private static final int FOUR = 4;
+
     private App() {
     }
 
     /**
-     * Java Runtime Shutdown Hook demo
-     * 
+     * Java Runtime Shutdown Hook demo.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) throws Exception {
-        
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
                 System.out.println("TERM");
                 main.interrupt();
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < FOUR; i++) {
                     System.out.println("busy");
                     try {
-                        Thread.sleep(1000);
-                    } catch (Exception e) {}
+                        Thread.sleep(THOUSAND);
+                    } catch (InterruptedException e) { }
                 }
                 System.out.println("exit");
             }
@@ -33,7 +35,7 @@ public final class App {
 
         main = Thread.currentThread();
         while (true) {
-            Thread.sleep(1000);
+            Thread.sleep(THOUSAND);
             System.out.println("run");
         }
 
